8b1e1b0d12e958980bc2a0b2728c932505a72a60
AustinD123 <64781636+AustinD123@users.noreply.github.com>
2023-12-01 09:53:00 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from time_1 import time
-
-
+from conversion import conversion
 
 root = tk.Tk()
 root.geometry("500x500")
@@ -18,7 +17,7 @@ label.grid( column=0, columnspan=2, padx=40, pady=40)
 # Set a fixed width and external padding for all buttons
 button_style = {'style': 'Accent.TButton', 'padding': 20, 'width': 20,}
 
-button1 = ttk.Button(root, text='UNIT CONVERSION', **button_style)
+button1 = ttk.Button(root, text='UNIT CONVERSION', **button_style, command=conversion)
 button1.grid(row=1,  pady=(0, 10))
 
 button2 = ttk.Button(root, text='TIME CONVERSION', **button_style, command=time)
@@ -29,5 +28,20 @@ button3.grid(row=3, pady=(0, 10))
 
 button4 = ttk.Button(root, text='TEMPERATURE CONVERSION', **button_style)
 button4.grid(row=4,)
+def change_theme():
+    # NOTE: The theme's real name is azure-<mode>
+    if root.tk.call("ttk::style", "theme", "use") == "azure-dark":
+        # Set light theme
+        root.tk.call("set_theme", "light")
+    else:
+        # Set dark theme
+        root.tk.call("set_theme", "dark")
+
+# Remember, you have to use ttk widgets
+button = ttk.Button(root, text="Change theme!", command=change_theme)
+button.grid(row=10, pady=10)
+
+close_button = ttk.Button(root, text="Close", command=root.destroy)
+close_button.grid( pady=10)
 
 root.mainloop()
